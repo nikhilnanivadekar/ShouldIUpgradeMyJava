15f141e4f71d033aaa3a5c988a62cd649d040b87
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2022-09-21 20:13:27 -0700

    Add tests for var

---
+++ code-examples/src/test/java/nikhil/nani/code/examples/ExamplesTest.java
@@ -0,0 +1,20 @@
+package nikhil.nani.code.examples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class ExamplesTest {
+
+    @Test
+    public void var() {
+
+        Map<Integer, String> map = Map.of(1, "One", 2, "Two");
+
+        var withVar = Map.of(1, "One", 2, "Two");
+
+        Assertions.assertEquals(map, withVar);
+    }
+
+}
