fa48438338ac0021e85204c0be8690765b053538
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2022-09-22 21:30:07 -0700

    Trivial formatting fix.

--- performance-tests-jdk8/src/main/java/nikhil/nani/MemoryTestsJdk8.java
+++ performance-tests-jdk8/src/main/java/nikhil/nani/MemoryTestsJdk8.java
@@ -24,6 +24,7 @@ public class MemoryTestsJdk8 {
 
                 printMemoryUtilizationUsingRuntime("String", size);
             }
+            System.out.println("-----------------------------------------------------------");
         }
     }
 
