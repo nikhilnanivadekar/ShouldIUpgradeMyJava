52aa84af03b8430af5a7e0fe2b22be1a16eb907a
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2022-09-28 20:32:32 -0700

    Update content for switch

--- code-examples/src/test/java/nikhil/nani/code/examples/ExamplesSwitchTest.java
+++ code-examples/src/test/java/nikhil/nani/code/examples/ExamplesSwitchTest.java
@@ -35,6 +35,20 @@ public class ExamplesSwitchTest {
         Assertions.assertEquals(this.getQuarterOldSwitch(12), this.getQuarterNewSwitch(12));
         Assertions.assertEquals(this.getQuarterOldSwitch(13), this.getQuarterNewSwitch(13));
 
+        Assertions.assertEquals(this.getQuarterNewSwitch(1), this.getQuarterNewSwitchAsExpression(1));
+        Assertions.assertEquals(this.getQuarterNewSwitch(2), this.getQuarterNewSwitchAsExpression(2));
+        Assertions.assertEquals(this.getQuarterNewSwitch(3), this.getQuarterNewSwitchAsExpression(3));
+        Assertions.assertEquals(this.getQuarterNewSwitch(4), this.getQuarterNewSwitchAsExpression(4));
+        Assertions.assertEquals(this.getQuarterNewSwitch(5), this.getQuarterNewSwitchAsExpression(5));
+        Assertions.assertEquals(this.getQuarterNewSwitch(6), this.getQuarterNewSwitchAsExpression(6));
+        Assertions.assertEquals(this.getQuarterNewSwitch(7), this.getQuarterNewSwitchAsExpression(7));
+        Assertions.assertEquals(this.getQuarterNewSwitch(8), this.getQuarterNewSwitchAsExpression(8));
+        Assertions.assertEquals(this.getQuarterNewSwitch(9), this.getQuarterNewSwitchAsExpression(9));
+        Assertions.assertEquals(this.getQuarterNewSwitch(10), this.getQuarterNewSwitchAsExpression(10));
+        Assertions.assertEquals(this.getQuarterNewSwitch(11), this.getQuarterNewSwitchAsExpression(11));
+        Assertions.assertEquals(this.getQuarterNewSwitch(12), this.getQuarterNewSwitchAsExpression(12));
+        Assertions.assertEquals(this.getQuarterNewSwitch(13), this.getQuarterNewSwitchAsExpression(13));
+
         Assertions.assertEquals(this.getQuarterNewSwitch(1), this.getQuarterYieldSyntax(1));
         Assertions.assertEquals(this.getQuarterNewSwitch(2), this.getQuarterYieldSyntax(2));
         Assertions.assertEquals(this.getQuarterNewSwitch(3), this.getQuarterYieldSyntax(3));
@@ -48,6 +62,19 @@ public class ExamplesSwitchTest {
         Assertions.assertEquals(this.getQuarterNewSwitch(11), this.getQuarterYieldSyntax(11));
         Assertions.assertEquals(this.getQuarterNewSwitch(12), this.getQuarterYieldSyntax(12));
         Assertions.assertEquals(this.getQuarterNewSwitch(13), this.getQuarterYieldSyntax(13));
+
+        Assertions.assertEquals(this.getQuarterNewSwitch(1), this.getQuarterEnum(Months.JAN));
+        Assertions.assertEquals(this.getQuarterNewSwitch(2), this.getQuarterEnum(Months.FEB));
+        Assertions.assertEquals(this.getQuarterNewSwitch(3), this.getQuarterEnum(Months.MAR));
+        Assertions.assertEquals(this.getQuarterNewSwitch(4), this.getQuarterEnum(Months.APR));
+        Assertions.assertEquals(this.getQuarterNewSwitch(5), this.getQuarterEnum(Months.MAY));
+        Assertions.assertEquals(this.getQuarterNewSwitch(6), this.getQuarterEnum(Months.JUN));
+        Assertions.assertEquals(this.getQuarterNewSwitch(7), this.getQuarterEnum(Months.JUL));
+        Assertions.assertEquals(this.getQuarterNewSwitch(8), this.getQuarterEnum(Months.AUG));
+        Assertions.assertEquals(this.getQuarterNewSwitch(9), this.getQuarterEnum(Months.SEP));
+        Assertions.assertEquals(this.getQuarterNewSwitch(10), this.getQuarterEnum(Months.OCT));
+        Assertions.assertEquals(this.getQuarterNewSwitch(11), this.getQuarterEnum(Months.NOV));
+        Assertions.assertEquals(this.getQuarterNewSwitch(12), this.getQuarterEnum(Months.DEC));
     }
 
     private String getQuarterOldSwitch(Integer monthNumber) {
@@ -93,6 +120,17 @@ public class ExamplesSwitchTest {
         };
     }
 
+    private String getQuarterNewSwitchAsExpression(Integer monthNumber) {
+        final String quarter = switch (monthNumber) {
+            case 1, 2, 3 -> "Q1";
+            case 4, 5, 6 -> "Q2";
+            case 7, 8, 9 -> "Q3";
+            case 10, 11, 12 -> "Q4";
+            default -> "Not a month in year";
+        };
+        return quarter;
+    }
+
     private String getQuarterYieldSyntax(Integer monthNumber) {
 
         return switch (monthNumber) {
@@ -104,6 +142,20 @@ public class ExamplesSwitchTest {
                 yield "Q4";
             }
             default -> "Not a month in year";
+        };
+    }
+
+    public enum Months {
+        JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+    }
+
+    private String getQuarterEnum(Months month) {
+
+        return switch (month) {
+            case JAN, FEB, MAR -> "Q1";
+            case APR, MAY, JUN -> "Q2";
+            case JUL, AUG, SEP -> "Q3";
+            case OCT, NOV, DEC -> "Q4";
         };
     }
 }
